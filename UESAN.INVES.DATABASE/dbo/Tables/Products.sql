94775ce3243112084c6898f86db75663de0deeef
J03-M4NT <jmantillah1503@gmail.com>
2025-05-16 20:34:23 -0500

    Database dummy

---
+++ UESAN.INVES.DATABASE/dbo/Tables/Products.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Products] (
+    [Id]          INT             IDENTITY (1, 1) NOT NULL,
+    [Description] NVARCHAR (100)  NULL,
+    [UnitPrice]   DECIMAL (18, 2) NULL,
+    [Status] BIT NULL, 
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
