86b248729ac9b41cf94a9fa7b40708dfc6f99ac4
Jefferson <Jefferson@JEFFFERSON>
2025-05-30 17:26:30 -0500

    JTW token agregado

---
+++ UESAN.INVES.CORE/Core/Settings/JWTSettings.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using UESAN.INVES.CORE.Core.Interfaces;
+namespace UESAN.INVES.CORE.Core.Settings
+{
+    public class JWTSettings : IJWTSettings
+    {
+        public string SecretKey { get; set; }
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public double DurationInMinutes { get; set; }
+
+    }
+
+}
