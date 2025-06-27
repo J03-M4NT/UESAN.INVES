6dd88685f3744ed407d7ae63bafd6c81e0d2ee8a
Jefferson <Jefferson@JEFFFERSON>
2025-06-27 17:24:19 -0500

    fix: Actualizado los archivos de Publicaciones para que funcione el envio de archivos al frontend

--- UESAN.INVES.CORE/Core/DTOs/PublicacionesDTO.cs
+++ UESAN.INVES.CORE/Core/DTOs/PublicacionesDTO.cs
@@ -7,9 +7,20 @@ namespace UESAN.INVES.CORE.Core.DTOs
     {
         public int PublicacionId { get; set; }
         public string? Titulo { get; set; }
+        public string? NombreCategoria { get; set; }
+        public decimal? IncentivoUSD { get; set; }
+        public DateTime? FechaPublicacion { get; set; }
+    }
+
+    /*
+    public class PublicacionesDTO
+    {
+        public int PublicacionId { get; set; }
+        public string? Titulo { get; set; }
         public string? Contenido { get; set; }
         public DateTime? FechaPublicacion { get; set; }
     }
+    */
 
     // DTO solo con el ID: útil para eliminar, buscar, etc.
     public class PublicacionesIdDTO
